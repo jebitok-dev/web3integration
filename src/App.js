51b284581ff22880ef2496e59a51a1b02bbf0a7e
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-03-23 23:31:00 +0300

    Add Contract ABI and Address to the Frontend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import {useState} from "react";
+import {Contract, utils, ethers} from "ethers";
+import {useState, useEffect} from "react";
 import Footer from "./Components/Footer/Footer";
 import {Header} from "./Components/header/header";
 import MyStake from "./Components/MyStake/MyStake";
 import StakeHistory from "./Components/StakeHistory/StakeHistory";
+import "./App.css";
+import BRTTokenAbi from "./Utils/Web3/abi.json";
 
 export default function App() {
   const [connected, setConnected] = useState(false);
@@ -18,12 +21,13 @@ export default function App() {
   const [withdrawInput, setWithdrawInput] = useState("");
 
   const connectWallet = async () => {
-    setConnected(true);
-    setUserInfo({
-      matic_balance: "672345678909934",
-      token_balance: "123677793456798",
-      address: "0x34d9297629323795CE29190159206cDD81e6B2d2",
-    });
+    if (!!windows.ethereum || !!windows.web3) {
+      await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+    } else {
+      alert("please use an ethereum address");
+    }
   };
 
   const [stakeHistory, setStakeHistory] = useState([
@@ -59,10 +63,36 @@ export default function App() {
     },
   ]);
 
+  useEffect(() => {
+    if (!window.ethereum) return;
+    window.ethereum.on("connect", (payload) => {
+      if (Number(payload.chainId) !== 80001)
+        return alert(
+          "you are not on the right network, please switch to mumbai polygon"
+        );
+      const provider = new ethers.provider.Web3Provider(window.ethereum);
+      const account = await provider.listAccounts();
+      const userMaticBal = await provider.getBalance(account(0));
+      const BRTContractInstance = new Contract(
+        "0x67dBAF6D282E42F1374300284d439222C08D8dd2",
+        BRTTokenAbi,
+        provider
+      );
+      const userBRTBalance = await BRTContractInstance.balance01(account[0]);
+      // console.log("first", userBRTBalance)
+      setUserInfo({
+        matic_balance: userMaticBal,
+        token_balance: userBRTBalance,
+        address: account[0],
+      });
+      setConnected(true);
+    });
+  }, []);
+
   const onChangeInput = ({target}) => {
-    switch(target.id) {
+    switch (target.id) {
       case "stake":
-        setStakeInput(target.value)
+        setStakeInput(target.value);
         break;
 
       case "unstake":
@@ -72,17 +102,17 @@ export default function App() {
       default:
         break;
     }
-  }
+  };
 
   const onClickStake = (e) => {
-    e.preventDefault()
-    console.log("staking....", stakeInput)
-  }
+    e.preventDefault();
+    console.log("staking....", stakeInput);
+  };
 
   const onClickWithdraw = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     console.log("unstaking....", withdrawInput);
-  }
+  };
 
   return (
     <div>
